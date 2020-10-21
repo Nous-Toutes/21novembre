afea0e6b1e9dcbce32162c30dd42de3ca690e3d2
Lucas Geitner <lucas.geitner@outlook.com>
2020-10-21 12:12:45 +0200

    💅🏻 lint

--- functions/src/event/event-http.ts
+++ functions/src/event/event-http.ts
@@ -12,8 +12,8 @@ export const joinEvent = functions.region('europe-west3').https.onRequest(async 
 	response.header('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Methods', 'POST');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.set('Access-Control-Max-Age', '3600');
+	response.set('Access-Control-Allow-Headers', 'Content-Type');
+	response.set('Access-Control-Max-Age', '3600');
 
 	/* eslint-disable-next-line node/no-unsupported-features/es-syntax */
 	return (await import('./join-event')).default(request, response);
@@ -25,9 +25,9 @@ export const quitEvent = functions.region('europe-west3').https.onRequest(async 
 	response.header('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Methods', 'POST');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
+	response.set('Access-Control-Allow-Headers', 'Content-Type');
 	response.set('Access-Control-Max-Age', '3600');
-	
+
 	/* eslint-disable-next-line node/no-unsupported-features/es-syntax */
 	return (await import('./quit-event')).default(request, response);
 });
@@ -37,8 +37,8 @@ export const candidatEvent = functions.runWith({memory: '1GB'}).region('europe-w
 	response.header('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Origin', '*');
 	response.set('Access-Control-Allow-Methods', 'POST');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.set('Access-Control-Max-Age', '3600');
+	response.set('Access-Control-Allow-Headers', 'Content-Type');
+	response.set('Access-Control-Max-Age', '3600');
 
 	/* eslint-disable-next-line node/no-unsupported-features/es-syntax */
 	return (await import('./candidat-event')).default(request, response);
