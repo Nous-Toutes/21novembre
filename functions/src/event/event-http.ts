1d227d73eb55280b9505009d09afae6b68474fdd
Lucas Geitner <lucas.geitner@outlook.com>
2020-10-23 16:16:48 +0200

    🚉 Add a stat events

--- functions/src/event/event-http.ts
+++ functions/src/event/event-http.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import {events, STATUS} from "../utils/model"
+import {all, query, where} from "typesaurus"
 
 export const eventsFunction = functions.region('europe-west3').https.onRequest(async (request, response) => {
 	response.set('Access-Control-Allow-Origin', '*');
@@ -51,3 +53,22 @@ export const candidatEvent = functions.runWith({memory: '1GB'}).region('europe-w
 	return (await import('./candidat-event')).default(request, response);
 });
 
+
+
+export const Stats = functions.runWith({memory: '1GB'}).region('europe-west3').https.onRequest(async (request, response) => {
+	const all_events = await all(events)
+	const validateEvent = await query(events, [where("status", '==', STATUS.VALIDATE)])
+
+	const validateEventLength = validateEvent.length 
+	// calcul the sum
+	let sum = 0
+
+	all_events.forEach((a) => { 
+
+		let number = Number(a.data.number_of_people)
+		if (Number.isInteger(number)){
+			sum += Number(a.data.number_of_people) 
+		}
+	})
+	return response.json({personne_inscrite: sum, evenement_valide: validateEventLength});
+})
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-import {eventsFunction, joinEvent, candidatEvent, quitEvent} from './event/event-http';
+import {eventsFunction, joinEvent, candidatEvent, quitEvent, Stats} from './event/event-http';
 import {seedEvent} from './utils/seed';
 import {subscribed_to_newsletter, subscribed_leader_to_newsletter} from './email/subscribe-feminist-hook';
 const events = eventsFunction;
 import {scheduledFunction} from './utils/healther';
 
-export {events, joinEvent, subscribed_to_newsletter, candidatEvent, subscribed_leader_to_newsletter, scheduledFunction, seedEvent, quitEvent};
+export {events, joinEvent, subscribed_to_newsletter, candidatEvent, subscribed_leader_to_newsletter, scheduledFunction, seedEvent, quitEvent, Stats};
